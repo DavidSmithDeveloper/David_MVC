674777cde7b1d6df7ffada0dd4c68c53ced226e3
DavidSmithDeveloper <145584723+DavidSmithDeveloper@users.noreply.github.com>
2024-02-12 10:49:48 -0500

    ProjectUpdate

---
+++ David.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace David.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DavidProjectRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using DavidProjectRazor_Temp.Data;
+using DavidProjectRazor_Temp.Migrations;
+using DavidProjectRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DavidProjectRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+        public List<Category>CategoryList { get; set; }
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+    }
+}
